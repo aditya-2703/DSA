60c7b1ad09d5a0bf4bfac14977f5a9a8cda3f677
Aditya Chauhan <66458303+aditya-2703@users.noreply.github.com>
2021-02-16 20:21:22 +0530

    Update REVERSELINKEDLIST.py

--- LINKED_LIST/REVERSELINKEDLIST.py
+++ LINKED_LIST/REVERSELINKEDLIST.py
@@ -6,10 +6,39 @@ class Node:
         self.data=data
         self.next=None
     
-class Reverse_linkedlist:
+class linked_list:
     def __init__(self):
         self.start=None
-    def Reverse(self,root):
-        pass
+    def insert_end(self,data):
+        node=Node(data)
+        if self.start==None:
+            self.start=node
+            return 
+        temp=self.start
+        while temp.next:
+            temp=temp.next
+        temp.next=node
+    def print(self):
+        temp=self.start
+        data=""
+        while temp!=None:
+            data+=str(temp.data)+"->"
+            temp=temp.next
+        print(data)
+    def Reverse(self):
+        curr=self.start
+        prev=None
+        while curr!=None:
+            next=curr.next
+            curr.next=prev
+            prev=curr
+            curr=next
+        self.start=prev
+if __name__ == '__main__':
+    obj=linked_list()
 
-
+    for i in range(11):
+        obj.insert_end(i)
+        # obj.print()
+    obj.Reverse()
+    obj.print()
