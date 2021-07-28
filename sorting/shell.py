00cddb8afc7f981eaa2e40752eb92e3db54809a0
Aditya Chauhan <66458303+aditya-2703@users.noreply.github.com>
2021-07-28 09:31:21 +0530

    Update shell.py

--- sorting/shell.py
+++ sorting/shell.py
@@ -1,36 +1,26 @@
-#locho.
-def insertion_sort(list):
-    for i in range(1,len(list)):
-        temp=list[i]
-        j=i-1
-        while j>=0 and list[j]>temp:
-            list[j+1]=list[j]
-            j=j-1
-        list[j+1]=temp
+def shellSort(array, n):
+#     gap n-> n//2 -> n//4
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = array[i]
+            j = i
+            while j >= gap and array[j - gap] > temp:
+                array[j] = array[j - gap]
+                j -= gap
 
-def shell_sort(array,n):
-    gap=n//2
-    for i in range(gap,1,-1):
-        for j in range(n//2,):
-            for i in range(j-gap,0,-1):
-                if array[i+gap]>array[i]:
-                    break
-                else:
-                    array[i+gap],array[i]=array[i],array[i+gap]
-                i=i-gap
-            j+=1
-        gap//=2
-        if gap==1:
-            insertion_sort(array)
+            array[j] = temp
+        gap //= 2
 
+
+        
 
 if __name__ == '__main__':
-    array=[123,124,32,4,32,524,5,345,435,5]
-    print('ARRAY BEFORE SORTING : ',array)
-    shell_sort(array,len(array)-1)
-    print('ARRAY AFTER SORTING : ', array)
-
-
+    data = [9, 8, 3, 7, 5, 6, 4, 1]
+    size = len(data)
+    shellSort(data, size)
+    print('Sorted Array in Ascending Order:')
+    print(data)
 
 
 
