35196db8745d835531527d32b4c35f12bdf1774f
Aditya Chauhan <66458303+aditya-2703@users.noreply.github.com>
2022-01-26 16:41:24 +0530

    Update fibonacci.py

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -1,20 +1,30 @@
-def fibonacci(n):
-    dic_fib=dict()
-    # if n in dic_fib:return dic_fib[n]
-    # if n<2:
-    #     return n
-    # # else:
-    #     f=fibonacci(n-1)+fibonacci(n-2)
-    #     dic_fib[n]=f
-    #     print(dic_fib,dic_fib[n])
-    #     return dic_fib[n]
-    for i in range(1,n):
-        if i in dic_fib:
-            return dic_fib[i]
-        if i<2:
-            return n
-        else:
-            f=fibonacci(i-1)+fibonacci(i-2)
-            dic_fib[i]=i
+# he Fibonacci numbers are the numbers in the following integer sequence.
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+
+# In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation 
+# Fn = Fn-1 + Fn-2
+# with seed values 
+# F0 = 0 and F1 = 1.
+# Given a number n, print n-th Fibonacci Number. 
+# Examples: 
+# Input  : n = 2
+# Output : 1
+
+# Input  : n = 9
+# Output : 34
+
+def fibonacci(n,dp):
+    if n<0:
+        return 0
+    if n==0 or n==1:
+        return 1
+    if dp[n]!=0:
+        return dp[n]
+    value = fibonacci(n-1,dp) + fibonacci(n-2,dp)
+    dp[n] = value
+    return value
+    
 if __name__ == '__main__':
-    print(fibonacci(5))
+    n=5
+    dp = [0]*(n+1)
+    print(fibonacci(n,dp))
